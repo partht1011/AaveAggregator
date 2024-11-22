995908d2bf378f4b4befdfd30a7d4240d4ef4f85
snow <partht1011@gmail.com>
2024-11-22 05:55:32 -0800

    init infrastructure(aave aggregator func implement)

---
+++ contracts/AaveAggregator.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// Importing necessary OpenZeppelin contracts and Aave interfaces
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@aave/core-v3/contracts/interfaces/IPool.sol";
+
+contract AaveAggregator {
+    using SafeERC20 for IERC20;
+
+    // State variables
+    IPool public immutable aavePool; // Aave lending pool interface
+    IERC20 public immutable dai;       // DAI token interface
+
+    uint256 public totalShare = 0;    // Total shares of all users
+    mapping(address => uint256) public userShare; // Shares per user
+
+    /**
+     * @dev Constructor to initialize the Aave pool and DAI token.
+     * @param _aavePool Address of the Aave lending pool.
+     * @param _dai Address of the DAI token contract.
+     */
+    constructor(IPool _aavePool, IERC20 _dai) {
+        aavePool = _aavePool;
+        dai = _dai;
+    }
+
+    /**
+     * @dev Deposit DAI into the Aave pool and mint shares for the user.
+     * @param amount The amount of DAI to deposit.
+     * @notice The caller must approve the contract to spend DAI before calling this function.
+     */
+    function deposit(uint256 amount) external {
+        require(amount > 0, "Invalid deposit amount");
+
+        uint256 prevBalance = _balanceOf(); // Get previous aToken balance
+
+        // Transfer DAI from the user to the contract
+        dai.safeTransferFrom(msg.sender, address(this), amount);
+        dai.approve(address(aavePool), amount); // Approve Aave to spend DAI
+        
+        // Supply DAI to the Aave pool
+        aavePool.supply(address(dai), amount, address(this), 0);
+
+        // Calculate the user's share based on their contribution
+        uint256 share = (totalShare == 0) ? amount : (totalShare * amount) / prevBalance;
+
+        // Update user's shares and total shares
+        userShare[msg.sender] += share;
+        totalShare += share;
+    }
+
+    /**
+     * @dev Withdraw DAI from the Aave pool based on the user's shares.
+     * @param share The amount of shares to withdraw.
+     * @notice The caller must have enough shares to withdraw the requested amount.
+     */
+    function withdraw(uint256 share) external {
+        require(share > 0, "Nothing to withdraw");
+        require(share <= userShare[msg.sender], "Withdraw amount exceeds balance");
+
+        uint256 amount = getAmount(share); // Get the amount of DAI to withdraw
+
+        // Withdraw the underlying asset (DAI) from Aave
+        dai.approve(msg.sender, amount);
+        aavePool.withdraw(address(dai), amount, msg.sender);
+
+        // Update user's shares and total shares
+        userShare[msg.sender] -= share;
+        totalShare -= share;
+    }
+
+    /**
+     * @notice Get the amount of share of user
+     */
+    function getShare() external view returns (uint256) {
+        return userShare[msg.sender];
+    }
+
+    /**
+     * @dev Get the amount of DAI corresponding to a given share.
+     * @param share The number of shares to convert to DAI.
+     * @return The amount of DAI equivalent to the given shares.
+     */
+
+    function getAmount(uint256 share) public view returns (uint256) {
+        return (_balanceOf() * share) / totalShare; // Proportional amount based on shares
+    }
+
+    /**
+     * @dev Internal function to get the total balance of aTokens held by this contract.
+     * @return The balance of aTokens for the DAI reserve.
+     */
+    function _balanceOf() internal view returns (uint256) {
+        address aDaiAddress = aavePool.getReserveData(address(dai)).aTokenAddress; // Get aToken address
+        return IERC20(aDaiAddress).balanceOf(address(this)); // Return balance of aTokens
+    }
+}
---
+++ test/AaveAggregator.ts
@@ -0,0 +1,92 @@
+import { Address } from './../typechain-types/@openzeppelin/contracts/utils/Address';
+import { expect } from "chai"
+import { IERC20, IPool } from 'typechain-types';
+import { AaveAggregator } from '../typechain-types/contracts/AaveAggregator';
+import { ethers, network } from 'hardhat';
+async function timeTravel(seconds: number) {
+    await ethers.provider.send("evm_increaseTime", [seconds]);
+    await ethers.provider.send("evm_mine");
+}
+describe("AaveAggregator", function () {
+    const ADDRESS_AAVE_POOL = '0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2';
+    const ADDRESS_DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
+    const knownDAIHolder = "0xD1668fB5F690C59Ab4B0CAbAd0f8C1617895052B";
+
+    let daiHolder: any, user1: any, user2: any;
+    let aavePool: IPool;
+    let dai: IERC20;
+    let aggregator: AaveAggregator;
+    before(async () => {
+
+        [, user1, user2] = await ethers.getSigners();
+        aavePool = await ethers.getContractAt('IPool', ADDRESS_AAVE_POOL);
+        dai = await ethers.getContractAt('IERC20', ADDRESS_DAI);
+
+        const aggregatorFactory = await ethers.getContractFactory('AaveAggregator');
+        aggregator = await aggregatorFactory.deploy(ADDRESS_AAVE_POOL, ADDRESS_DAI);
+
+        await network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: [knownDAIHolder],
+        });
+
+        daiHolder = await ethers.getSigner(knownDAIHolder);
+        const amount = ethers.parseEther("1000"); // Amount of DAI to transfer
+        await dai.connect(daiHolder).transfer(user1, amount);
+        await dai.connect(daiHolder).transfer(user2, amount);
+
+        await network.provider.request({
+            method: "hardhat_stopImpersonatingAccount",
+            params: [knownDAIHolder],
+        });
+
+    })
+    it("check user1 and user2 have sufficient amount of DAI", async () => {
+        expect(await dai.balanceOf(user1)).to.be.equal(ethers.parseEther("1000"));
+        expect(await dai.balanceOf(user2)).to.be.equal(ethers.parseEther("1000"));
+    });
+
+    it("deposit amount must be more than 0", async () => {
+        await dai.connect(user1).approve(await aggregator.getAddress(), ethers.parseEther('100'))
+        await expect(aggregator.connect(user1).deposit(0)).to.be.revertedWith("Invalid deposit amount")
+
+        console.log("   User1 amount: ", ethers.formatEther(await dai.balanceOf(user1)));
+        console.log("   User2 amount: ", ethers.formatEther(await dai.balanceOf(user2)));
+    })
+
+    it("user1 and user2 deposit DAI to the pool", async () => {
+        await dai.connect(user1).approve(await aggregator.getAddress(), ethers.parseEther('100'))
+        await aggregator.connect(user1).deposit(ethers.parseEther('100'))
+
+        await timeTravel(60 * 60 * 24 * 10)
+
+        await dai.connect(user2).approve(await aggregator.getAddress(), ethers.parseEther('100'))
+        await aggregator.connect(user2).deposit(ethers.parseEther('100'))
+
+        const user1Share = await aggregator.connect(user1).getShare()
+        const user2Share = await aggregator.connect(user2).getShare()
+
+        console.log("   User1 amount: ", ethers.formatEther(await dai.balanceOf(user1)));
+        console.log("   User2 amount: ", ethers.formatEther(await dai.balanceOf(user2)));
+
+        expect(user1Share).to.be.gt(user2Share)
+    })
+
+    it("user1 and user2 withdraw DAI from the pool", async () => {
+        await timeTravel(60 * 60 * 24 * 365)
+
+        const user1Share = await aggregator.connect(user1).getShare()
+        await aggregator.connect(user1).withdraw(user1Share)
+
+        const user2Share = await aggregator.connect(user2).getShare()
+        await aggregator.connect(user2).withdraw(user2Share)
+
+        const user1Amount = await dai.balanceOf(user1);
+        const user2Amount = await dai.balanceOf(user2);
+
+        console.log("   User1 amount: ", ethers.formatEther(user1Amount));
+        console.log("   User2 amount: ", ethers.formatEther(user2Amount));
+
+        expect(user1Amount).to.be.gt(user2Amount)
+    })
+});
